829cf0c8d22edf705cd957431c0af205171b6a54
cr7parker <romalezhnin@gmail.com>
2018-06-30 16:56:06 +0700

    getRandomSeconds

--- unique-content-creator/UoW/fetch.js
+++ unique-content-creator/UoW/fetch.js
@@ -2,12 +2,13 @@ const
   request = require('request'),
   cheerio = require('cheerio');
 
+
 module.exports = function fetch(url) {
   return new Promise(function (resolve, reject) {
     request({
       url,
       method: 'GET',
-      timeout: 10000
+      timeout: 20000
     }, function (error, response, body) {
       if (error) return reject(error);
       return resolve(cheerio.load(body));
